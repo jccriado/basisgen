aea962ae2bec048dbcc5ff7ae32602b2f4bce146
Juan Carlos Criado Alamo <jccc01@correo.ugr.es>
2019-01-06 19:04:06 +0100

    Specify more data in setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,21 @@
 from distutils.core import setup
 
+with open('README.md', mode='r') as readme_file:
+    readme = readme_file.read()
+
 setup(
     name='basisgen',
-    version='0.3'
+    version='0.3',
+
+    description='Package for the generation of operator bases',
+    long_description=readme,
+
+    url='https://github.com/jccriado/basisgen',
+
+    author='Juan Carlos Criado',
+    author_email='jccriadoalamo@ugr.es',
+
+    license='MIT',
+
+    packages=['basisgen'],
 )
