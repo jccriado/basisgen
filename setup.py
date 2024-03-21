c8d752d711e71808a57f71c5eff6f8f08625a09e
Juan Carlos Criado <jccriadoalamo@ugr.es>
2024-03-21 12:58:11 +0100

    update version number

--- setup.py
+++ setup.py
@@ -1,27 +1,21 @@
 from distutils.core import setup
 
 setup(
-    name='basisgen',
-    version='1.0.2',
-
-    description='Package for the generation of operator bases',
-
-    url='https://github.com/jccriado/basisgen',
-
-    author='Juan Carlos Criado',
-    author_email='jccriadoalamo@ugr.es',
-
-    license='MIT',
-
-    packages=['basisgen'],
-
+    name="basisgen",
+    version="1.0.3",
+    description="Package for the generation of operator bases",
+    url="https://github.com/jccriado/basisgen",
+    author="Juan Carlos Criado",
+    author_email="jccriadoalamo@ugr.es",
+    license="MIT",
+    packages=["basisgen"],
     classifiers=[
-        'Intended Audience :: Science/Research',
-        'License :: OSI Approved :: MIT License',
-        'Operating System :: OS Independent',
-        'Programming Language :: Python :: 3',
-        'Programming Language :: Python :: 3.5',
-        'Topic :: Scientific/Engineering :: Mathematics',
-        'Topic :: Scientific/Engineering :: Physics',
+        "Intended Audience :: Science/Research",
+        "License :: OSI Approved :: MIT License",
+        "Operating System :: OS Independent",
+        "Programming Language :: Python :: 3",
+        "Programming Language :: Python :: 3.5",
+        "Topic :: Scientific/Engineering :: Mathematics",
+        "Topic :: Scientific/Engineering :: Physics",
     ],
 )
